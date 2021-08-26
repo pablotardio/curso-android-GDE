44230786e2825312aef50fd415226690ec891b0b
pablotardio <pablo.tv30@gmail.com>
2021-08-26 18:18:34 -0400

    2nd practice :added screen for calculatin notes

---
+++ app/src/main/java/com/example/myfirstlearningapp/screen_calculator.java
@@ -0,0 +1,14 @@
+package com.example.myfirstlearningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class screen_calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_calculator);
+    }
+}
