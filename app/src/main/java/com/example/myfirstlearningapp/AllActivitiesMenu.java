77a2b4e270de91ea763d27246ca800ee5dc6d4b1
pablotardio <pablo.tv30@gmail.com>
2021-09-14 20:46:15 -0400

    Menu to jump to every other activity in the course

---
+++ app/src/main/java/com/example/myfirstlearningapp/AllActivitiesMenu.java
@@ -0,0 +1,60 @@
+package com.example.myfirstlearningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class AllActivitiesMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_activities_menu);
+    }
+    public void navigateToMain(View view){
+        Intent intent= new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+    public void navigateToRadioCalculator(View view){
+        Intent intent= new Intent(this,RadioCalculator.class);
+        startActivity(intent);
+    }
+    public void navigateToScreenCalculator(View view){
+        Intent intent= new Intent(this,screen_calculator.class);
+        startActivity(intent);
+    }public void navigateToSpinnerSelect(View view){
+        Intent intent= new Intent(this,SpinnerSelectCalculator.class);
+        startActivity(intent);
+    }public void navigateToListViewNames(View view){
+        Intent intent= new Intent(this,ListViewNames.class);
+        startActivity(intent);
+    }
+    public void navigateToChecklistCalculator(View view){
+        Intent intent= new Intent(this,CheckListCalculator.class);
+        startActivity(intent);
+    }
+    public void navigateToImageButton(View view){
+        Intent intent= new Intent(this,ImageButtonTesting.class);
+        startActivity(intent);
+    }
+    public void navigateToFormValidation(View view){
+        Intent intent= new Intent(this,form_validation.class);
+        startActivity(intent);
+    }
+
+    public void navigateToPasarDatoAOtraPantalla(View view){
+        Intent intent= new Intent(this,PasarDatoAOtraPantalla.class);
+        startActivity(intent);
+    }
+
+    public void navigateToWebView(View view){
+        Intent intent= new Intent(this,WebViewDataWrite.class);
+        startActivity(intent);
+    }
+    public void navigateToSharedPrefs(View view){
+        Intent intent= new Intent(this,SharedPreferencesTest.class);
+        startActivity(intent);
+    }
+}
