38620df9fc1f394e2eb49244cf68a69695e8cea2
pablotardio <pablo.tv30@gmail.com>
2021-09-14 23:38:28 -0400

    Example 28: created class to use SQLite with their implementations

---
+++ app/src/main/java/com/example/myfirstlearningapp/AdminSQLiteHelper.java
@@ -0,0 +1,25 @@
+package com.example.myfirstlearningapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+//step 1 import SQLiteOpenHelper and extend your class to it
+//tip: Alt+enter to fix the extends problems
+public class AdminSQLiteHelper  extends SQLiteOpenHelper
+{
+    public AdminSQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table articulos(id int primary key, descripcion text,precio real)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
