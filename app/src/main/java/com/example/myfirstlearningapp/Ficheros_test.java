92bfc5fb0fa2ea5e08e49983b6f42465265101ce
pablotardio <pablo.tv30@gmail.com>
2021-09-14 22:35:09 -0400

    Example 28: Ficheros View Done

---
+++ app/src/main/java/com/example/myfirstlearningapp/Ficheros_test.java
@@ -0,0 +1,14 @@
+package com.example.myfirstlearningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ficheros_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ficheros_test);
+    }
+}
