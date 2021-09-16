d9797ea58080dcbc21048168eb222b26160b6105
pablotardio <pablo.tv30@gmail.com>
2021-09-16 19:09:42 -0400

    Example 37: ScrollView view and logic done (not connected with menu) 2nd commit of all the corrections

--- app/src/main/java/com/example/myfirstlearningapp/ScrollViewTest.java
+++ app/src/main/java/com/example/myfirstlearningapp/ScrollViewTest.java
@@ -3,6 +3,8 @@ package com.example.myfirstlearningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ScrollViewTest extends AppCompatActivity {
 
@@ -10,5 +12,52 @@ public class ScrollViewTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll_view_test);
+    }
+    public void handlePressFruta(View view){
+        switch (view.getId()){
+            case R.id.imageButtonFruitBanana:
+                Toast.makeText(this, "Esto es una Banana", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitCereza:
+                Toast.makeText(this, "Esto es una cereza", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitFrambuesa:
+                Toast.makeText(this, "Esto es una Frambuesa", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitFresa:
+                Toast.makeText(this, "Esto es una Fresa", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitKiwi:
+                Toast.makeText(this, "Esto es una Kiwi", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitMango:
+                Toast.makeText(this, "Esto es una Mango", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitManzana:
+                Toast.makeText(this, "Esto es una Manzana", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitMelon:
+                Toast.makeText(this, "Esto es una Melon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitNaranja:
+                Toast.makeText(this, "Esto es una Naranja", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitPera:
+                Toast.makeText(this, "Esto es una Pera", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitPina:
+                Toast.makeText(this, "Esto es una Piña", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitSandia:
+                Toast.makeText(this, "Esto es una Sandia", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitUva:
+                Toast.makeText(this, "Esto es una Uva", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonFruitZarzamora:
+                Toast.makeText(this, "Esto es una Zarzamora", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
     }
 }
